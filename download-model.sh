34d0122a37b64e6090c72a4169aa1faf8c9af68f
Julian Bilcke <julian.bilcke@huggingface.co>
2023-10-06 22:36:15 +0200

    SDXL exploded the server (+50 gb), let's use another strategy

--- download-model.sh
+++ download-model.sh
@@ -8,10 +8,18 @@ cd $modelsDir
 if [ ! -d "$modelsDir/$modelName" ]
 then
     # make sure we have LFS capability
-    git lfs install
+    # git lfs install
 
     # clone the repo (note: it is huge)
-    git clone https://huggingface.co/stabilityai/$modelName
+    # git clone https://huggingface.co/stabilityai/$modelName
+
+    # edit: actually let's not download the huge git repo of SDXL
+    # but only the unet
+    mkdir $modelName
+    cd $modelName
+    wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/unet/diffusion_pytorch_model.safetensors
+    cd ..
 else
     echo "$modelName already exists"
 fi
+cd ..
